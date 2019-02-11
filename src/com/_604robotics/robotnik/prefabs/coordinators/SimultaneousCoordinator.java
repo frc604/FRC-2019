df60f4b45a65b9f29e450cf27b46d2a260ffdb6f
InfinityPhase <InfinityPhase@users.noreply.github.com>
2019-02-11 14:38:50 -0800

    SimultaneousCoordinator that should work, without garunteeing start or end timing

--- src/com/_604robotics/robotnik/prefabs/coordinators/SimultaneousCoordinator.java
+++ src/com/_604robotics/robotnik/prefabs/coordinators/SimultaneousCoordinator.java
@@ -1,111 +1,58 @@
 package com._604robotics.robotnik.prefabs.coordinators;
 
-import java.util.ArrayList;
-
 import com._604robotics.robotnik.Coordinator;
+import com._604robotics.robotnik.Logger;
 import com._604robotics.robotnik.utils.annotations.Untested;
 
-@Deprecated @Untested("Straight-up doesn't work")
+import java.util.HashMap;
+import java.util.Map;
+
+@Untested("Does not guarantee the start or finish time of any Coordinator")
 public class SimultaneousCoordinator extends Coordinator {
-    
-    private ArrayList<Coordinator> coordinators;
-    private ArrayList<Boolean> started;
-    private ArrayList<Boolean> stopped;
-    private int index;
-    private boolean finished;
-    private boolean fullCycle;
+	private final Logger logger;
+	private Map<Coordinator, Boolean[]> allTasks;
 
-    public SimultaneousCoordinator(Coordinator... coords) {
-    	coordinators = new ArrayList<Coordinator>();
-    	started = new ArrayList<Boolean>();
-    	stopped = new ArrayList<Boolean>();
-        for (Coordinator co: coords) {
-            addCoordinator(co);
-            started.add(false);
-            stopped.add(false);
-        }
-        index = 0;
-        finished = false;
-        fullCycle = false;
-    }
-    
-    public void addCoordinator(Coordinator coord) {
-        coordinators.add(coord);
-    }
+	public SimultaneousCoordinator( Coordinator[] tasks ) {
+		this.logger = new Logger( SimultaneousCoordinator.class);
+		this.allTasks = new HashMap<>();
+		for( Coordinator c : tasks ) {
+			this.allTasks.put( c, new Boolean[]{false, false} );
+		}
+	}
 
-    @Override
-    protected void begin() {
-    	for( int f=0; f<started.size(); f++ ) {
-    		started.set(f, false);
-    		stopped.set(f, false);
-    	}
-    	index = 0;
-    	finished = false;
-    	fullCycle = false;
-    }
+	@Override
+	public void begin() {
+		logger.info("Begin");
+		for( Coordinator c : allTasks.keySet() ) {
+			logger.info("Starting " + c.toString());
+			c.start();
+		}
+	}
 
-    @Override
-    protected boolean run() {
-    	boolean finishedStarting = true;
-    	for( boolean startCheck : started ) {
-    		finishedStarting &= startCheck;
-    	}
-    	
-    	boolean currentState = false;
-    	boolean stoppedState = true;
-    	
-    	if( finished ) {
-    		stoppedState = false;
-    		currentState = true;
-    		for( int f=0; f<stopped.size(); f++ ) {
-    			if( !stopped.get(f) ) {
-    				stopped.set(f, true);
-    				coordinators.get(f).stop();
-    				System.out.println("Stopped "+coordinators.get(f).toString());
-    				stoppedState = true;
-    				break;
-    			}
-    		}
-    	} else if( finishedStarting ) {
-    		int modulatedIndex = index%coordinators.size();
-    		fullCycle |= coordinators.get(modulatedIndex).execute();
-    		System.out.println("Executed " + coordinators.get(modulatedIndex).toString());
-    		if( !(modulatedIndex == coordinators.size()-1 && !fullCycle) ) {
-    			fullCycle = false;
-    			currentState = true;
-    		}
-    		index++;
-    	} else {
-    		currentState = true;
-    		for( int f=0; f<started.size(); f++ ) {
-    			if( !started.get(f) ) {
-    				started.set(f, true);
-    				coordinators.get(f).start();
-    				System.out.println("Started " + coordinators.get(f).toString());
-    				break;
-    			}
-    		}
-    	}
-    	
-    	if( !currentState ) {
-    		finished = true;
-    	}
-    	
-    	return stoppedState;
-    	
-//        boolean currentstate=false;
-//        // Continue running until all are done
-//        for (Coordinator co:coordinators) {
-//            System.out.println("Running co "+co.toString());
-//            // execute does nothing if the coordinator is in stopped state
-//            currentstate = currentstate || co.execute();
-//        }
-//        return currentstate;
-    }
+	@Override
+	public boolean run() {
+		boolean running = false;
 
-    @Override
-    protected void end() {
-        // Do nothing
-    }
+		for( Coordinator c : allTasks.keySet() ) {
+			if( c.execute() ) {
+				running = true;
+			}
+		}
+
+		return running;
+	}
+
+	@Override
+	public void end() {
+		for( Coordinator c : allTasks.keySet() ) {
+			c.stop();
+			logger.info("Stopped " + c.toString());
+		}
+		logger.info("End");
+	}
+
+	public void addCoordinator( Coordinator c ) {
+		allTasks.put( c, new Boolean[]{false,false} );
+	}
 
 }
