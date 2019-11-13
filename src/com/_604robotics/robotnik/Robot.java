d63bce9782b0c0639ee03ba72e4e2a213c725396
cttdev <cttewari@gmail.com>
2019-11-13 01:07:35 -0800

    Switch SmapleRobot to RobotBase.

--- src/com/_604robotics/robotnik/Robot.java
+++ src/com/_604robotics/robotnik/Robot.java
@@ -13,12 +13,15 @@ import java.util.zip.InflaterInputStream;
 
 import com._604robotics.robotnik.utils.Pair;
 
+import edu.wpi.first.hal.HAL;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 
-@SuppressWarnings("deprecation")
-public abstract class Robot extends SampleRobot {
+public abstract class Robot extends RobotBase {
     public static double DEFAULT_REPORT_INTERVAL = 5;
 
     private static final Logger logger = new Logger(Robot.class);
@@ -79,28 +82,23 @@ public abstract class Robot extends SampleRobot {
         return testMode;
     }
 
-    @Override
-    protected void robotInit () {
+    public void robotInit () {
         printBanner("eNqlVsGO3CAMvfMV3DIrZPtu9VNGwj1sL1UVaXuoVsrH1zYkgYTMrGbJikDAj4f97NkQQrxuwiwM/TcGYJBwbVEMkLCxIbE14EtLao/RIwARtW9AzHpsm+LzJhRWmP42+NyWpUOCFWnWu+5LSM+RGqZIzPN6H2B3XOFCXwBSc6iW5ri0e7V4zD5Dki0o6krYuR7munE9HBoXz/HoMeJqXhhKgupb3oArpiTHEY0h09HFtBEBN9x4xqPWXAmAQV0NKEPdNBHuPYw81IESUm5yZrUSr8dgGeA+HwcCiIvzbTBUT3E2d3M+3bRGlXBV198vKPOhaqu6dhm83DA5K6GetAYXrbM8jfzZHIOfGglA15iu9gS0GChamqEEVx/ybBINZnGhvnlnDYlqnNm2wOzCcUNvs2lW5JjTCVo4zSqpaRdbOKZjBZFThSqJwKmFs3jXBGjgZuxqTex1vlGrCPqW+t4Q6omnQre6XrzmDAFU0+sxCcNAh02NHgHskpR5YH8Qkrjvk4nCWi+XYZHfy8TDQp0uqjxe5Fe348HPUkuxZ6yOg/DwR5CrhC2OFQjta3hyl+IavzbHUsq+k4YwTd9BwEkhVIBvL0OQYWhPjxDycJx9fMsULwB0g/3luMQl2951XFZu+U1nvjC0X+KUFTzHu43iPdZxzmW+GBZFujS3pxyyeL+Ol7g+Mf4wL8bpbK274l0J05LRaPjYerfP/tyzkfJEzxrKcxR+ffz88/5v/vj9NIyTQUymh/BqIPUfBZgUJLxq7xRes765A0II/wGVOKOq");
         printBanner("eJzjUqAR0OPCL1+DTxRDEiIQEw8CmhCKOPPjE4Cs+HiFGBgbyMEiAKT0gUhXtwxkNFAowYYo8zXArBowCWbHgI2uAUOEJBDYIOnV1MdvONh/IKN04mviQS6EsYFAH+RaZEmwbAxCszoXEAAAFJdXwA==");
     }
 
-    @Override
     public void autonomous () {
         loop("Autonomous", autonomousMode, () -> isEnabled() && isAutonomous());
     }
 
-    @Override
     public void operatorControl () {
         loop("Teleop", teleopMode, () -> isEnabled() && isOperatorControl());
     }
 
-    @Override
     public void test () {
         loop("Test", testMode, () -> isEnabled() && isTest());
     }
 
-    @Override
     protected void disabled () {
         loop("Disabled", null, this::isDisabled);
     }
@@ -168,6 +166,51 @@ public abstract class Robot extends SampleRobot {
         }
 
         logger.info(name + " mode end");
+    }
+    
+    @Override
+    public void startCompetition() {
+        robotInit();
+
+        // Tell the DS that the robot is ready to be enabled
+        HAL.observeUserProgramStarting();
+
+        while (true) {
+            if (isDisabled()) {
+                m_ds.InDisabled(true);
+                disabled();
+                m_ds.InDisabled(false);
+
+                while (isDisabled()) {
+                    Timer.delay(0.01);
+                }
+            } else if (isAutonomous()) {
+                m_ds.InAutonomous(true);
+                autonomous();
+                m_ds.InAutonomous(false);
+                while (isAutonomous() && !isDisabled()) {
+                    Timer.delay(0.01);
+                }
+            } else if (isTest()) {
+                LiveWindow.setEnabled(true);
+                Shuffleboard.enableActuatorWidgets();
+                m_ds.InTest(true);
+                test();
+                m_ds.InTest(false);
+                while (isTest() && isEnabled()) {
+                    Timer.delay(0.01);
+                }
+                LiveWindow.setEnabled(false);
+                Shuffleboard.disableActuatorWidgets();
+            } else {
+                m_ds.InOperatorControl(true);
+                operatorControl();
+                m_ds.InOperatorControl(false);
+                while (isOperatorControl() && !isDisabled()) {
+                    Timer.delay(0.01);
+                }
+            }
+        } /* while loop */
     }
 
     private void printBanner (String banner) {
