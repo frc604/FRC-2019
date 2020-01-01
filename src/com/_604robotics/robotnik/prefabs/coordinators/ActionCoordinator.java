afda6cbd059b24cfd26742fda881a11cd684498f
cttdev <cttewari@gmail.com>
2020-01-01 10:37:02 -0800

    Additional Coordinators.

--- src/com/_604robotics/robotnik/prefabs/coordinators/ActionCoordinator.java
+++ src/com/_604robotics/robotnik/prefabs/coordinators/ActionCoordinator.java
@@ -1,93 +1,91 @@
 package com._604robotics.robotnik.prefabs.coordinators;
 
+import static edu.wpi.first.wpilibj.util.ErrorMessages.requireNonNullParam;
+
 import com._604robotics.robotnik.Coordinator;
 import com._604robotics.robotnik.prefabs.flow.SmartTimer;
 
-import static edu.wpi.first.wpilibj.util.ErrorMessages.requireNonNullParam;
-
-/** 
- * A class that allows a runnable to be run as a coordinator. It can be used to inline macros in autonomus code.
+/**
+ * A class that allows a runnable to be run as a coordinator. It can be used to inline macros in
+ * autonomus code.
  */
-public class ActionCoordinator extends Coordinator{
-    private Runnable action;
-    private Type type;
+public class ActionCoordinator extends Coordinator {
+  private Runnable action;
+  private Type type;
 
-    private SmartTimer timer = new SmartTimer();
+  private SmartTimer timer = new SmartTimer();
 
-    private double time;
+  private double time;
 
-    
-    /** 
-     * Creates an Action Coordinator that runs the runnable for the specfied ammount of time.
-     * 
-     * @param action Runnable to run.
-     * @param time Time to run runnable for.
-     */
-    public ActionCoordinator(Runnable action, double time){
-        this.action = requireNonNullParam(action, "action", "ActionCoordinator");
+  /**
+   * Creates an Action Coordinator that runs the runnable for the specfied ammount of time.
+   *
+   * @param action Runnable to run.
+   * @param time Time to run runnable for.
+   */
+  public ActionCoordinator(Runnable action, double time) {
+    this.action = requireNonNullParam(action, "action", "ActionCoordinator");
 
-        if (time <= 0) {
-            throw new IllegalArgumentException("Must run for a positive amount of time!");
-        }
-        
-        this.time = time;
-
-        type = Type.TIMED;
+    if (time <= 0) {
+      throw new IllegalArgumentException("Must run for a positive amount of time!");
     }
 
-    /** 
-     * Creates an Action Coordinator that runs the runnable once.
-     * 
-     * @param action Runnable to run.
-     */
-    public ActionCoordinator(Runnable action){
-        this.action = requireNonNullParam(action, "action", "ActionCoordinator");
+    this.time = time;
 
-        type = Type.INSTANT;
+    type = Type.TIMED;
+  }
+
+  /**
+   * Creates an Action Coordinator that runs the runnable once.
+   *
+   * @param action Runnable to run.
+   */
+  public ActionCoordinator(Runnable action) {
+    this.action = requireNonNullParam(action, "action", "ActionCoordinator");
+
+    type = Type.INSTANT;
+  }
+
+  /** Creates an empty Action Coordinator that runs nothing. Useful for debugging. */
+  public ActionCoordinator() {
+    type = Type.EMPTY;
+  }
+
+  @Override
+  protected void begin() {
+    switch (type) {
+      case TIMED:
+        timer.start();
+        break;
+      case INSTANT:
+        action.run();
+        break;
+      case EMPTY:
+        break;
+    }
+  }
+
+  @Override
+  protected boolean run() {
+    boolean running = false;
+
+    switch (type) {
+      case TIMED:
+        action.run();
+        running = !timer.hasReachedTime(time);
+        break;
+      case INSTANT:
+        break;
+      case EMPTY:
+        break;
     }
 
-    /** 
-     * Creates an empty Action Coordinator that runs nothing. Useful for debugging.
-     */
-    public ActionCoordinator(){
-        type = Type.EMPTY;
-    }
+    return running;
+  }
 
-    @Override
-    protected void begin() {
-        switch(type){
-            case TIMED:
-                timer.start();
-                break;
-            case INSTANT:
-                action.run();
-                break;
-            case EMPTY:
-                break;
-        }
-    }
-
-    @Override
-    protected boolean run() {
-        boolean running = false;
-
-        switch(type){
-            case TIMED:
-                action.run();
-                running = !timer.hasReachedTime(time);
-                break;
-            case INSTANT:
-                break;
-            case EMPTY:
-                break;
-        }
-
-        return running;
-    }
-    
-    public enum Type {
-        TIMED,
-        INSTANT,
-        EMPTY
-      }
+  public enum Type {
+    TIMED,
+    INSTANT,
+    EMPTY
+  }
 }
